1b2464be6caf6da5554cad97d257330203c994fe
user <ahkakok@gmail.com>
2025-01-25 19:11:48 +0300

    Add message to basic middleware

--- middlewares/basic/basic.go
+++ middlewares/basic/basic.go
@@ -1,10 +1,15 @@
 package basic_middleware
 
-import "Gone/core"
+import (
+	"Gone/core"
+	"Gone/custom_types"
+	"strconv"
+)
 
 type BasicMiddleware struct {
 }
 
 func (self *BasicMiddleware) Handle(context core.ITransferData) {
-
+	context.SetResponseBody("Hello world!")
+	context.SetResponseHeader("Content-Length", custom_types.HeaderValue(strconv.Itoa(len("Hello world!"))))
 }
