7984ead69fc0329a49b53a11e241b0474b1c83cb
user <ahkakok@gmail.com>
2025-01-25 19:12:49 +0300

    Add main.go content

--- main.go
+++ main.go
@@ -1,4 +1,15 @@
 package main
 
+import (
+	"Gone/hosting"
+	basic_middleware "Gone/middlewares/basic"
+	"Gone/transfering/tcptext"
+	"net"
+)
+
 func main() {
+	host := hosting.UnsynchronisedHost{}
+	listener, _ := net.Listen("tcp", "localhost:8080")
+	host.Setup(&tcptext.TcpTextTransferer{Listener: listener}, &basic_middleware.BasicMiddleware{})
+	host.Start()
 }
