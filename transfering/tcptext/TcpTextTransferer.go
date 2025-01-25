fc71519de2313e34f930e5e7aa7b349eebfb125d
user <ahkakok@gmail.com>
2025-01-25 04:14:30 +0300

    Implement EndTransfer and NextData

--- transfering/tcptext/TcpTextTransferer.go
+++ transfering/tcptext/TcpTextTransferer.go
@@ -3,13 +3,18 @@ package tcptext
 import (
 	"Gone/core"
 	. "Gone/custom_types"
+	"net"
+	"strings"
 )
 
 type TextTransferData struct {
+	http_method      string
+	url              string
 	request_headers  Headers
 	response_headers Headers
 	request_body     string
 	response_body    string
+	connection       *TextTransferConnection
 }
 
 func (self *TextTransferData) GetRequestHeaders() Headers {
@@ -55,16 +60,65 @@ func (self *TextTransferData) GetResponseBody() string {
 }
 
 func (self *TextTransferData) EndTransfer() {
-	panic("Not implemented.")
+	res := "HTTP/1.1 200 OK"
+	for k, v := range self.response_headers {
+		res += "\n"
+		res += string(k) + ": " + string(v)
+	}
+	res += "\n\n"
+	res += self.response_body
+	self.connection.TcpConnection.Write([]byte(res))
 }
 
 type TextTransferConnection struct {
-	// TODO: Implement
+	TcpConnection net.Conn
 }
 
 func (self *TextTransferConnection) NextData() core.ITransferData {
-	panic("Not implemented.")
+	req := ""
+	buf := make([]byte, 1024)
+	for {
+		n, err := self.TcpConnection.Read(buf)
+		if err != nil {
+			panic("something went wrong in next data method")
+		}
+		if n == 0 {
+			break
+		}
+		req += string(buf[:n])
+	}
+	lines := strings.Split(req, "\n")
+
+	http_method := strings.Split(lines[0], " ")[0]
+	url := strings.Split(lines[0], " ")[1]
+	headers := Headers{}
+
+	i := 1
+	for ; i < len(lines) && lines[i] != ""; i++ {
+		split_idx := strings.Index(lines[1], ": ")
+		headers[HeaderName(lines[i][:split_idx])] = HeaderValue(lines[i][split_idx+2:])
+	}
+
+	body := ""
+	i++
+	for ; i < len(lines); i++ {
+		body += lines[i]
+		if i+1 < len(lines) {
+			body += "\n"
+		}
+	}
+
+	return &TextTransferData{
+		http_method:      http_method,
+		url:              url,
+		request_headers:  headers,
+		response_headers: Headers{},
+		request_body:     body,
+		response_body:    "",
+		connection:       self,
+	}
 }
+
 func (self *TextTransferConnection) IsClosed() bool {
 	panic("Not implemented.")
 }
